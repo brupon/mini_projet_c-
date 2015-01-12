959f0c4981136b36a3df8743430769ed946247ee
Florentin Ponzio <florentinponzio@MacBook-Pro-de-Florentin.local>
2015-01-12 14:35:10 +0100

    Initial Commit

---
+++ mini_projet_c++/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  mini_projet_c++
+//
+//  Created by Florentin Ponzio on 12/01/2015.
+//  Copyright (c) 2015 Florentin Ponzio. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
